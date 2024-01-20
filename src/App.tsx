971da6260595e1b03ca77eee822e145d3b9a2780
Cesar <ccordero.007@gmail.cim>
2024-01-20 12:10:45 -0600

    Building the App Layout, sidebar and header'

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
@@ -14,24 +15,26 @@ export default function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route
-            index
-            element={<Navigate replace to="dashboard" />} />
-          <Route
-            path="dashboard"
-            element={<Dashboard />} />
-          <Route
-            path="Bookings"
-            element={<Bookings />} />
-          <Route
-            path="users"
-            element={<Users />} />
-          <Route
-            path="settings"
-            element={<Settings />} />
-          <Route
-            path="account"
-            element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route
+              index
+              element={<Navigate replace to="dashboard" />} />
+            <Route
+              path="dashboard"
+              element={<Dashboard />} />
+            <Route
+              path="Bookings"
+              element={<Bookings />} />
+            <Route
+              path="users"
+              element={<Users />} />
+            <Route
+              path="settings"
+              element={<Settings />} />
+            <Route
+              path="account"
+              element={<Account />} />
+          </Route>
           <Route
             path="login"
             element={<Login />} />
