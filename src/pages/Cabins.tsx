bc5e4e230f7af4566f6afcf647a411a59a21c95c
Cesar <ccordero.007@gmail.cim>
2024-01-20 16:19:27 -0600

    Install React Query and setting it

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,11 +1,4 @@
-import { useEffect } from "react"
-import { getCabins } from "../services/apiCabins"
-
 const Cabins = () => {
-
-    useEffect(() => {
-        getCabins().then((data) => console.log(data));
-    }, [])
 
     return (
         <div>Cabins</div>
